7b7f83f227e5940f462561db658f9b62a38d4f28
lucascs20182 <lucascs20182@gmail.com>
2021-02-19 21:06:18 -0300

    add notations with br names, reqs and ranges

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelloWorldAspNetMVC.Models
 {
     public class Category
     {
         public int id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição deve ser preenchido.")]
         public string description { get; set; }
 
         public List<Product> Products { get; set; }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HelloWorldAspNetMVC.Models
 {
     public class Product
     {
         public int id { get; set; }
+
+        [Display(Name = "Descrição")]
         public string description { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Range(1, 10, ErrorMessage = "A qtd. máxima permitida é 10 e a mínima, 1.")]
         public int amount { get; set; }
+
         public int categoryId { get; set; }
+
+        [Display(Name = "Categoria")]
         public Category Category {get; set; }
     }
 }
